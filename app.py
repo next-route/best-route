91b96703478ca242bf6254b51bf13f18b4c8403f
Ana Carla Vasconcelos <anacarlavgs@gmail.com>
2021-10-21 22:32:15 -0300

    create CRUD for Truck class

--- app.py
+++ app.py
@@ -5,30 +5,41 @@ import json
 
 from sqlalchemy.orm import exc
 
+# Inicializa o Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:rota_mySQL_1509@localhost:3306/melhor_rota'
+# Modificar "host", "senha" e "0000" pelas informações correspondentes no seu mysql
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://host:senha@localhost:0000/melhor_rota'
 
+# Conecta o Flask ao db(database) através da instância SLQAlchemy
 db = SQLAlchemy(app)
+
+# Cria a classe Truck e declara as variáveis como colunas no banco de dados
 
 
 class Truck(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.Boolean)
     location = db.Column(db.String(50))
+    destination = db.Column(db.String(50))
 
+    # retorna os valores das variáveis em formato json
     def to_json(self):
-        return{"id": self.id, "status": self.status, "location": self.location}
+        return{"id": self.id, "status": self.status, "location": self.location, "destination": self.destination}
 
-# select all
+# seleciona todos os trucks cadastrados
 
 
 @app.route("/trucks", methods=['GET'])
 def get_trucks():
+    # variável que chama todos os atributos da classe truck
     trucks_object = Truck.query.all()
+    # trandforma em json
     trucks_json = [truck.to_json() for truck in trucks_object]
 
     return generate_response(200, "trucks", trucks_json)
+
+# gera a resposta ao método http(GET, POST, PUT e DELETE)
 
 
 def generate_response(status, content_name, content, message=False):
@@ -39,7 +50,7 @@ def generate_response(status, content_name, content, message=False):
         body["message"] = message
     return Response(json.dumps(body), status=status, mimetype="application/json")
 
-# select one
+# Seleciona apenas um truck pelo seu id
 
 
 @app.route("/truck/<id>", methods=['GET'])
@@ -49,15 +60,18 @@ def get_truck(id):
 
     return generate_response(200, "truck", truck_json)
 
-# register truck
+# cadastra um truck colocando o status, localização e destino (id é autoincrementável)
 
 
 @app.route("/truck", methods=["POST"])
 def create_truck():
+    # faz requisição em json dos valores informados pelo usuário
     body = request.get_json()
 
     try:
-        truck = Truck(status=body["status"], location=body["location"])
+        truck = Truck(
+            status=body["status"], location=body["location"], destination=body["destination"])
+        # adiciona os valores informados no banco de dados
         db.session.add(truck)
         db.session.commit()
         return generate_response(201, "truck", truck.to_json(), "Truck created with success.")
@@ -65,7 +79,7 @@ def create_truck():
         print('Error', e)
         return generate_response(400, "truck", {}, "Error creating truck")
 
-# update truck
+# faz update de um truck, especificado pelo id
 
 
 @app.route("/truck/<id>", methods=["PUT"])
@@ -74,10 +88,13 @@ def update_truck(id):
     body = request.get_json()
 
     try:
+        # se, por exemplo, no body(valores informados) tenha status e localização, apenas esses valores serão modificados.
         if("status" in body):
             truck_object.status = body["status"]
         if("location" in body):
             truck_object.location = body["location"]
+        if("destination" in body):
+            truck_object.destination = body["destination"]
 
         db.session.add(truck_object)
         db.session.commit()
@@ -86,7 +103,7 @@ def update_truck(id):
         print('Error', e)
         return generate_response(400, "truck", {}, "Error updating truck")
 
-# delete truck
+# deleta um truck de acordo com o id
 
 
 @app.route("/truck/<id>", methods=["DELETE"])
